38954ab1b6f18db757c38aab2e12a3fbc181488d
emgiezet <maxmalecki@gmail.com>
2012-10-02 21:51:36 +0200

    configured phpunit tests

--- web/index.php
+++ web/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -19,18 +18,14 @@ $app->register(new FormServiceProvider());
 $app
 		->register(new Silex\Provider\MonologServiceProvider(),
 				array('monolog.logfile' => __DIR__ . '/../logs/development.log',));
-// $app
-// 		->register(new Silex\Provider\TranslationServiceProvider(),
-// 				array('locale_fallback' => 'en',));
 
 $app['facebook.class_path'] = __DIR__ . '/../vendor/facebook/php-sdk/src/';
 
-$app
-		->register(new Faceboo\Provider\FacebookServiceProvider(),
-				array('facebook.app_id' => '113291218705258',
-						'facebook.secret' => '9aceca728c59acaf00ab66b146339c81',
-						'facebook.namespace' => 'max-test'));
-
+$app->register(new Faceboo\Provider\FacebookServiceProvider(), array(
+		'facebook.app_id' => 'FB_APP_ID',
+		'facebook.secret' => 'FB_APP_SECRET',
+		'facebook.namespace' => 'FB_APPNAME'
+));
 $app
 		->register(new Silex\Provider\DoctrineServiceProvider(),
 				array(
@@ -39,16 +34,16 @@ $app
 
 $pages = array('/' => 'homepage', '/error' => 'error');
 
-$app
-		->before(
-				function (Request $request) use ($app) {
-					if ($response = $app['facebook']->protect())
-						return $response;
-				});
+// $app
+// 		->before(
+// 				function (Request $request) use ($app) {
+// 					if ($response = $app['facebook']->protect())
+// 						return $response;
+// 				});
 
 foreach ($pages as $route => $view) {
 	$app
-			->post($route,
+			->get($route,
 					function () use ($app, $view) {
 						if ($response = $app['facebook']->auth()) {
 							return $app['twig']->render($view . '.html.twig');
@@ -60,59 +55,3 @@ foreach ($pages as $route => $view) {
 }
 
 $app->run();
-=======
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Silex\Provider\FormServiceProvider;
-use Silex\Provider\MonologServiceProvider;
-use Silex\Provider\TranslationServiceProvider;
-use Faceboo\Provider\FacebookServiceProvider;
-use Symfony\Component\HttpFoundation\Request;
-$app = new Silex\Application();
-
-$app['debug'] = true;
-
-$app
-		->register(new Silex\Provider\TwigServiceProvider(),
-				array('twig.path' => __DIR__ . '/../src/resources/views',));
-$app->register(new FormServiceProvider());
-
-$app
-		->register(new Silex\Provider\MonologServiceProvider(),
-				array('monolog.logfile' => __DIR__ . '/../logs/development.log',));
-// $app
-// 		->register(new Silex\Provider\TranslationServiceProvider(),
-// 				array('locale_fallback' => 'en',));
-
-$app['facebook.class_path'] = __DIR__ .'/../vendor/facebook/php-sdk/src/';
-
-$app->register(new Faceboo\Provider\FacebookServiceProvider(), array(
-		'facebook.app_id' => 'FB_APP_ID',
-		'facebook.secret' => 'FB_APP_SECRET',
-		'facebook.namespace' => 'FB_APPNAME'
-));
-
-$pages = array('/' => 'homepage', 'error'=>'error');
-
-$app->before(function(Request $request) use ($app) {
-	if ($response = $app['facebook']->protect()) return $response;
-});
-
-foreach ($pages as $route => $view) {
-	$app
-			->get($route,
-					function () use ($app, $view) {
-						if ($response = $app['facebook']->auth())
-						{
-							return $app['twig']->render($view . '.html.twig');
-						}
-						else
-						{
-							return $app['twig']->render('error.html.twig');
-						}
-					})->bind($view);
-}
-
-$app->run();
->>>>>>> a27f729c79d7682066395252b363e48eac7b97f9

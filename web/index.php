c96a5e2ac4f6eea7edfa1a6b03fa13ac62cef3d0
emgiezet <maxmalecki@gmail.com>
2012-10-02 22:13:14 +0200

    formatting of the code

--- web/index.php
+++ web/index.php
@@ -11,26 +11,26 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 $app
-		->register(new Silex\Provider\TwigServiceProvider(),
-				array('twig.path' => __DIR__ . '/../src/resources/views',));
+        ->register(new Silex\Provider\TwigServiceProvider(),
+                array('twig.path' => __DIR__ . '/../src/resources/views',));
 $app->register(new FormServiceProvider());
 
 $app
-		->register(new Silex\Provider\MonologServiceProvider(),
-				array('monolog.logfile' => __DIR__ . '/../logs/development.log',));
+        ->register(new Silex\Provider\MonologServiceProvider(),
+                array('monolog.logfile' => __DIR__ . '/../logs/development.log',));
 
 $app['facebook.class_path'] = __DIR__ . '/../vendor/facebook/php-sdk/src/';
 
-$app->register(new Faceboo\Provider\FacebookServiceProvider(), array(
-		'facebook.app_id' => 'FB_APP_ID',
-		'facebook.secret' => 'FB_APP_SECRET',
-		'facebook.namespace' => 'FB_APPNAME'
-));
 $app
-		->register(new Silex\Provider\DoctrineServiceProvider(),
-				array(
-						'db.options' => array('driver' => 'pdo_sqlite',
-								'path' => __DIR__ . '/app.db',),));
+        ->register(new Faceboo\Provider\FacebookServiceProvider(),
+                array('facebook.app_id' => 'FB_APP_ID',
+                        'facebook.secret' => 'FB_APP_SECRET',
+                        'facebook.namespace' => 'FB_APPNAME'));
+$app
+        ->register(new Silex\Provider\DoctrineServiceProvider(),
+                array(
+                        'db.options' => array('driver' => 'pdo_sqlite',
+                                'path' => __DIR__ . '/app.db',),));
 
 $pages = array('/' => 'homepage', '/error' => 'error');
 
@@ -42,16 +42,17 @@ $pages = array('/' => 'homepage', '/error' => 'error');
 // 				});
 
 foreach ($pages as $route => $view) {
-	$app
-			->get($route,
-					function () use ($app, $view) {
-						if ($response = $app['facebook']->auth()) {
-							return $app['twig']->render($view . '.html.twig');
-						} else {
-							return $app['twig']->render('error.html.twig');
-						}
-						return $app['twig']->render($view . '.html.twig');
-					})->bind($view);
+    $app
+            ->get($route,
+                    function () use ($app, $view)
+                    {
+                        if ($response = $app['facebook']->auth()) {
+                            return $app['twig']->render($view . '.html.twig');
+                        } else {
+                            return $app['twig']->render('error.html.twig');
+                        }
+                        return $app['twig']->render($view . '.html.twig');
+                    })->bind($view);
 }
 
 $app->run();
